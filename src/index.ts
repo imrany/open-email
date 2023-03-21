3bf6821eff92ac42f85ded52a32fb36b095ec1db
imrany <imranmat254gmail.com>
2023-03-21 15:13:46 +0300

    working the routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
 import express from "express"
+import { sendInterface } from "./interfaces&types/int"
 
 const app=express()
+
+//route
+app.get('/',async(req,res):Promise<void>=>{
+    try {
+        const {mailfrom,mailto,subject,message}:sendInterface=req.body
+        res.status(200).send({msg:"Hello there"})
+    } catch (error:any) {
+        res.status(500).send({error:error.message})
+    }
+})
+
+app.post('/send',async(req,res):Promise<void>=>{
+    try {
+        const {mailfrom,mailto,subject,message}:sendInterface=req.body
+        res.status(200).send({msg:"Hello there"})
+    } catch (error:any) {
+        res.status(500).send({error:error.message})
+    }
+})
 
 //listening to port
 let port:number|string=process.env.PORT||8000
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
+
---
+++ src/interfaces&types/int.ts
@@ -0,0 +1,7 @@
+
+export interface sendInterface{
+    mailfrom:string
+    mailto:string
+    subject:string
+    message:string
+}
