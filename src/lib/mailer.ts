552267ce83400b280ce1df4688940d592bc55ba0
imrany <imranmat254gmail.com>
2023-03-21 22:41:39 +0300

    working on the mailer function in lib

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -1,7 +1,29 @@
 import nodemailer from "nodemailer"
+import { mailDetailsInterface } from "../interfaces&types/int"
 import * as dotenv from "dotenv"
 dotenv.config()
 
-const mailer=nodemailer
+function mailer(email:string, subject:string, text:string){
+    let mailTransporter=nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:process.env.TRANSPORTER,
+            pass:process.env.PASSWORD
+        }
+    })
+    let details:mailDetailsInterface={
+        from:process.env.TRANSPORTER,
+        to:email,
+        subject,
+        text
+    }
+    mailTransporter.sendMail(details,(err: any)=>{
+        if(err){
+            return({error:"Mail wasn't sent, try again!"})
+        }else{
+            return({msg:"Mail sent"})
+        }
+    })
+}
 
 export default mailer
